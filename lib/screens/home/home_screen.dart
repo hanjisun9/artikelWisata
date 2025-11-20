900cef15cf1d37c273fdaed02994d5d8ebf5ef8f
jujuu <azizahnf85@gmail.com>
2025-11-20 22:29:44 +0700

    update7

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 import '../../widgets/grid_artikel_populer.dart';
 import '../../controllers/artikel_controller.dart';
 import '../../models/artikel_model.dart';
+import '../../widgets/grid_artikel_all.dart';
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
@@ -44,7 +45,7 @@ class HomeScreen extends StatelessWidget{
                       Icons.notifications,
                       color: Color(0XFFD1A824),
                       size: 30,
-                    )
+                    ),
                   ],
                 ),
                 SizedBox(height: 20),
@@ -57,43 +58,122 @@ class HomeScreen extends StatelessWidget{
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(50),
                       borderSide: BorderSide.none,
-                    )
+                    ),
                   ),
                   onChanged: (value) {
-                    print(
-                      'Kata kunci: $value',
-                    );
+                    print('Kata kunci: $value');
                   },
                 ),
                 SizedBox(height: 20),
                 Text(
                   'Populer',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                 ),
                 SizedBox(height: 10),
                 FutureBuilder(
-                  future: ArtikelController.getArtikel(), 
+                  future: ArtikelController.getArtikel(),
                   builder: (context, snapshot) {
                     if (snapshot.connectionState == ConnectionState.waiting) {
                       return const CircularProgressIndicator();
                     } else if (snapshot.hasError) {
                       return Text('Error: ${snapshot.error}');
-                    } else{
+                    } else {
                       final artikelList = snapshot.data ?? [];
-                      final List<Artikel> artikelPopuler = artikelList.take(4).toList();
+                      final List<Artikel> artikelPopuler = artikelList
+                          .take(4)
+                          .toList();
+                      final List<Artikel> artikelAll = artikelList.toList();
 
-                      return GridArtikelPopuler(artikelList: artikelPopuler);
+                      return Column(
+                        children: [
+                          GridArtikelPopuler(artikelList: artikelPopuler),
+                          SizedBox(height: 20),
+                          Container(
+                            width: double.infinity,
+                            height: 90,
+                            decoration: BoxDecoration(
+                              color: Color(0XFFD1A824),
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(20),
+                              child: Row(
+                                children: [
+                                  Image.asset(
+                                    'assets/icons/news-paper.png',
+                                    width: 40,
+                                    height: 40,
+                                  ),
+                                  SizedBox(width: 10),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        'Lihat Artikel Kamu',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18,
+                                        ),
+                                      ),
+                                      Text(
+                                        'Yuk, Mulai Buat Artikel Kamu Sendiri',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Spacer(),
+                                  CircleAvatar(
+                                    backgroundColor: Colors.white,
+                                    child: IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(
+                                        Icons.arrow_forward,
+                                        color: Color(0XFFD1A824),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Artikel Lainnya',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold
+                                ),
+                              ),
+                              Text(
+                                'See all',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Color(0XFFD1A824),
+                                ),
+                              )
+                            ],
+                          ),
+                          SizedBox(height: 10),
+                          GridArtikelAll(artikelList: artikelAll),
+                        ],
+                      );
                     }
-                  }
-                  ),
+                  },
+                ),
               ],
             ),
-            ),
+          ),
         ),
-      )
-      );
+      ),
+    );
   }
 }
---
+++ lib/widgets/grid_artikel_all.dart
@@ -0,0 +1,60 @@
+import 'package:artikel_wisata/models/artikel_model.dart';
+import 'package:flutter/material.dart';
+
+class GridArtikelAll extends StatelessWidget{
+  final List<Artikel> artikelList;
+  const GridArtikelAll({super.key, required this.artikelList});
+
+  @override
+  Widget build(BuildContext context) {
+    const baseUrl = 'https://api-pariwisata.rakryan.id';
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+        childAspectRatio: 1,
+      ),
+
+      itemCount: artikelList.length,
+      itemBuilder: (context, index) {
+        final artikel = artikelList[index];
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Container(
+                height: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                    image: NetworkImage('$baseUrl/${artikel.image}'),
+                    fit: BoxFit.cover
+                  )
+                ),
+              ),
+            ),
+            SizedBox(height: 5),
+            Text(
+              artikel.title,
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              ),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Text(
+              artikel.description,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(fontSize: 12),
+            )
+          ],
+        );
+      },
+    );
+  }
+}
